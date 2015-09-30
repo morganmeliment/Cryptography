5e365427f09bf958380b5f13f80619e97d3d5147
Morgan Meliment <morganmeliment@gmail.com>
2015-09-30 14:00:56 -0400

    Updated from Brython Server: September 30, 2015 2:01:12 PM EDT

--- cryptography.py
+++ cryptography.py
@@ -1,6 +1,6 @@
 """
 cryptography.py
-Author: <your name here>
+Author: Morgan Meliment
 Credit: <list sources used, if any>
 
 Assignment:
@@ -9,4 +9,19 @@ Write and submit a program that encrypts and decrypts user data.
 
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
+def repeatkey(mlen, klen):
+    if len(mlen) == len(klen):
+        return(klen)
+    elif len(mlen) > len(klen):
+        diff = len(mlen) - 
+    
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+start = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+if start != "e" and start != "d" and start != "q":
+    print("Did not understand command, try again.")
+elif start == "q":
+    print("Goodbye!")
+elif start == "e":
+    emes = input("Message: ")
+    key = input("Key: ")
+    
