f3f1795170c0a0d92072f7167f2e15bf0bff2dd3
Morgan Meliment <morganmeliment@gmail.com>
2015-10-05 14:09:03 -0400

    Updated from Brython Server: October 5, 2015 2:09:28 PM EDT

--- cryptography.py
+++ cryptography.py
@@ -23,44 +23,46 @@ def repeatkey(mlen, klen):
         return("".join(x for x in time) + klen[0:diff])
     elif len(mlen) < len(klen):
         return(klen[0:len(mlen)])
-
-associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
-start = input("Enter e to encrypt, d to decrypt, or q to quit: ")
-if start != "e" and start != "d" and start != "q":
-    print("Did not understand command, try again.")
-elif start == "q":
-    print("Goodbye!")
-elif start == "e":
-    emes = input("Message: ")
-    key = input("Key: ")
-    editkey = repeatkey(emes, key)
-    encr = []
-    isn = 0
-    for q in emes:
-        ind = associations.find(q)
-        twoind = associations.find(editkey[isn])
-        isn += 1
-        newin = ind + twoind
-        if newin > 84:
-            newin = newin % 85
-        encr.append(associations[newin])
-    print("".join(x for x in encr))
-elif start == "d":
-    emes = input("Message: ")
-    key = input("Key: ")
-    editkey = repeatkey(emes, key)
-    encr = []
-    isn = 0
-    for q in emes:
-        ind = associations.find(q)
-        twoind = associations.find(editkey[isn])
-        isn += 1
-        newin = ind - twoind
-        if newin < 0:
-            newin = newin + 85
-        encr.append(associations[newin])
-    print("".join(x for x in encr))
-    
+tare = true
+while tare:
+    associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+    start = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+    if start != "e" and start != "d" and start != "q":
+        print("Did not understand command, try again.")
+    elif start == "q":
+        print("Goodbye!")
+        break
+    elif start == "e":
+        emes = input("Message: ")
+        key = input("Key: ")
+        editkey = repeatkey(emes, key)
+        encr = []
+        isn = 0
+        for q in emes:
+            ind = associations.find(q)
+            twoind = associations.find(editkey[isn])
+            isn += 1
+            newin = ind + twoind
+            if newin > 84:
+                newin = newin % 85
+            encr.append(associations[newin])
+        print("".join(x for x in encr))
+    elif start == "d":
+        emes = input("Message: ")
+        key = input("Key: ")
+        editkey = repeatkey(emes, key)
+        encr = []
+        isn = 0
+        for q in emes:
+            ind = associations.find(q)
+            twoind = associations.find(editkey[isn])
+            isn += 1
+            newin = ind - twoind
+            if newin < 0:
+                newin = newin + 85
+            encr.append(associations[newin])
+        print("".join(x for x in encr))
+        
     
     
     
